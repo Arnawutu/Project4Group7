f71484b5255f079bc1e1a61e2bf585dc2f0417bf
MahsaNafei <mahsa.nafe@gmail.com>
2024-02-10 19:22:47 -0500

    update applicatin

--- app.py
+++ app.py
@@ -1,136 +1,101 @@
 from flask import Flask, render_template, request
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import confusion_matrix, classification_report
+from imblearn.over_sampling import RandomOverSampler
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.linear_model import LogisticRegression
-
-# Psycopg2 is a popular PostgreSQL adapter for the Python programming language. It allows Python code to interact with PostgreSQL databases. 
 import psycopg2
 from psycopg2 import sql
-
 
 # Initialize Flask application
 app = Flask(__name__)
 
-#############START  POSTGRES CONNECTION ########################
-#Info for the connection wiht postgres SQL in RDS
-PGEND_POINT = 'database-1.cfwmkaw8o6bp.us-east-1.rds.amazonaws.com' #End Point
-PGDATABASE_NAME ='heart_attack_prediction_db' #data base name 
+############# START POSTGRES CONNECTION ############
+# Info for the connection with PostgreSQL in RDS
+PGEND_POINT = 'database-1.cfwmkaw8o6bp.us-east-1.rds.amazonaws.com'  # End Point
+PGDATABASE_NAME = 'heart_attack_prediction_db'  # Database name
 PGUSER_NAME = 'postgres'
 PGPASSWORD = 'B00TC4MP'
-#Defining functions for connection and close connection
+
 
 def connect():
     conn_string = f"host={PGEND_POINT} port=5432 dbname={PGDATABASE_NAME} user={PGUSER_NAME} password={PGPASSWORD}"
     conn = psycopg2.connect(conn_string)
     print("Connected!")
-    
-    #Create a cursor object
     cursor = conn.cursor()
-    
     return conn, cursor
 
-#Close connection function definition
+
 def close_connection(conn, cursor):
     conn.commit()
     cursor.close()
     conn.close()
     print("Connection closed.")
 
+
 conn, cursor = connect()
 
-
-#SQL SELECT statement that retrieves all columns (*) from the table named heartattackprediction.
+# SQL SELECT statement to retrieve all columns (*) from the table named encodedtable2
 query_hat_all = sql.SQL("""
 SELECT * FROM encodedtable2;
 """)
-#Preparation of the database cursor to execute the SQL query specified by query_hat_all. 
-#Once the query is executed, the cursor will hold the result set (if any) 
-#returned by the database server.
 cur = conn.cursor()
 cur.execute(query_hat_all)
 
-#put all the data in heartattackprediction table into a data frame in pandas all the columns name appear
-encoded_df = pd.DataFrame(cur.fetchall(), columns=['Patient ID','Country', 'Capital','Age', 'Sex', 'Cholesterol',
-       'Heart Rate', 'Diabetes', 'Family History', 'Smoking', 'Obesity',
-       'Alcohol Consumption', 'Exercise Hours Per Week', 
-       'Previous Heart Problems', 'Medication Use', 'Stress Level',
-       'Sedentary Hours Per Day', 'Income', 'BMI', 'Triglycerides',
-       'Physical Activity Days Per Week', 'Sleep Hours Per Day', 'Continent',
-       'Hemisphere', 'Heart Attack Risk', 'Systolic Pressure',
-       'Diastolic Pressure', 'lat', 'long','Diet_Average','Diet_Healthy','Diet_Unhealthy'])
+# Put all the data in the encodedtable2 table into a DataFrame in pandas
+encoded_df = pd.DataFrame(cur.fetchall(), columns=['Patient ID', 'Country', 'Capital', 'Age', 'Sex', 'Cholesterol',
+                                                   'Heart Rate', 'Diabetes', 'Family History', 'Smoking', 'Obesity',
+                                                   'Alcohol Consumption', 'Exercise Hours Per Week',
+                                                   'Previous Heart Problems', 'Medication Use', 'Stress Level',
+                                                   'Sedentary Hours Per Day', 'Income', 'BMI', 'Triglycerides',
+                                                   'Physical Activity Days Per Week', 'Sleep Hours Per Day', 'Continent',
+                                                   'Hemisphere', 'Heart Attack Risk', 'Systolic Pressure',
+                                                   'Diastolic Pressure', 'lat', 'long', 'Diet_Average', 'Diet_Healthy',
+                                                   'Diet_Unhealthy'])
 
-
-# Call this function when you're done with your database operations
 close_connection(conn, cursor)
-############END POSTGRES CONNECTION ############
-
-# Load the trained model and scaler
-# Load the data
-#encoded_df = pd.read_json("https://aws-project-4.s3.ca-central-1.amazonaws.com/encoded_df.json")
+############ END POSTGRES CONNECTION ############
 
 # Separate features and target variable
-X = encoded_df.drop(['Exercise Hours Per Week',
-                     'Stress Level',
-                     'Sedentary Hours Per Day',
-                     'Income',
-                     'Physical Activity Days Per Week',
-                     'Sleep Hours Per Day',
-                     'Heart Attack Risk',
-                     'Diet_Average',
-                     'Diet_Healthy',
-                     'Diet_Unhealthy',
-                     'Country',
-                     'Capital',
-                     'lat',
-                     'long',
-                     'Continent',
-                     'Patient ID',
-                     'Hemisphere'], axis=1)
+X = encoded_df.drop(['Exercise Hours Per Week', 'Stress Level', 'Sedentary Hours Per Day', 'Income',
+                     'Physical Activity Days Per Week', 'Sleep Hours Per Day', 'Heart Attack Risk', 'Diet_Average',
+                     'Diet_Healthy', 'Diet_Unhealthy', 'Country', 'Capital', 'lat', 'long', 'Continent',
+                     'Patient ID', 'Hemisphere'], axis=1)
 y = encoded_df['Heart Attack Risk']
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Create an instance of RandomOverSampler
+oversampler = RandomOverSampler(random_state=42)
 
-# Creating StandardScaler instance
+# Resample the data
+X_resampled, y_resampled = oversampler.fit_resample(X, y)
+
+# Normalize the feature matrix X_resampled
 scaler = MinMaxScaler()
+X_scaled = scaler.fit_transform(X_resampled)
 
-# Fitting StandardScaler
+# Split the resampled data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_resampled, test_size=0.3, random_state=42)
+
+# Normalize the feature matrix after splitting
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Load the trained model
-# Define the pipeline including MinMaxScaler and LogisticRegression
-log_reg_pipe = Pipeline([
-    ('mms', MinMaxScaler()),
-    ('log_reg', LogisticRegression())
-])
+# Define the random forest classifier with default hyperparameters
+rf_model = RandomForestClassifier(random_state=42)
 
-# Define the parameters grid for GridSearchCV
-params = {
-    'log_reg__C': np.logspace(-3, 3, 7),
-    'log_reg__penalty': ['l2']  # Update to include only 'l2' penalty
-}
-
-# Initialize GridSearchCV with the pipeline, parameters, scoring, and cross-validation
-gs_log_reg = GridSearchCV(
-    log_reg_pipe,
-    param_grid=params,
-    scoring='accuracy',
-    cv=10
-)
-
-# Fit GridSearchCV on the training data
-gs_log_reg.fit(X_train_scaled, y_train)
-
+# Train the model on the entire training set with the best hyperparameters
+best_rf_model = RandomForestClassifier(max_depth=30, min_samples_leaf=4, min_samples_split=10, n_estimators=300,
+                                        random_state=42)
+best_rf_model.fit(X_train_scaled, y_train)
 
 # Define the route for the home page
 @app.route('/')
 def home():
     return render_template('heart_attack.html')
+
 
 # Define a route for prediction
 @app.route('/predict', methods=['POST'])
@@ -155,14 +120,14 @@ def predict():
 
         # Create a feature vector from the user input
         user_input = [[age, sex, cholesterol, heart_rate, diabetes, family_history, smoking, obesity,
-                    alcohol_consumption, previous_heart_problems, medication_use, bmi, triglycerides,
-                    systolic_pressure, diastolic_pressure]]
+                       alcohol_consumption, previous_heart_problems, medication_use, bmi, triglycerides,
+                       systolic_pressure, diastolic_pressure]]
 
         # Scale the feature vector using the scaler fitted on the training data
         user_input_scaled = scaler.transform(user_input)
 
         # Make a prediction
-        prediction = gs_log_reg.predict(user_input_scaled)
+        prediction = best_rf_model.predict(user_input_scaled)
 
         # Determine the prediction message
         if prediction == 1:
@@ -171,7 +136,7 @@ def predict():
             prediction_message = "Low risk of heart attack"
 
         # Get the test data accuracy
-        test_accuracy = round(gs_log_reg.score(X_test_scaled, y_test) * 100, 2)
+        test_accuracy = round(best_rf_model.score(X_test_scaled, y_test) * 100, 2)
 
 
         # Render the prediction result page with the prediction message and test accuracy
